124310059fdcd9349f42ed5bc41f9bb146dd61b4
Matt Clinton <matt@laralabs.uk>
2019-10-22 21:46:24 +0100

    Fix failing test

--- tests/TimezoneTest.php
+++ tests/TimezoneTest.php
@@ -145,7 +145,7 @@ class TimezoneTest extends TestCase
             $timezone = new \Carbon\CarbonTimeZone('UTC');
         }
 
-        $this->assertEquals($timezone, $converted->timezone);
+        $this->assertEquals($timezone->timezone, $converted->timezone->timezone);
     }
 
     /** @test */
